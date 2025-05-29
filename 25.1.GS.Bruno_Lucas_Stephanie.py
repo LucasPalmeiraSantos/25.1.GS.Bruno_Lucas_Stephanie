ff2c9f934b7a321cc0b42b8a4ac664866c55555a
Bruno Alves <128005195+brunoavss@users.noreply.github.com>
2025-05-29 09:25:13 -0300

    Atividade 1 -1.2.0v

--- 25.1.GS.Bruno_Lucas_Stephanie.py
+++ 25.1.GS.Bruno_Lucas_Stephanie.py
@@ -1,28 +1,33 @@
-# Bruno Alves de Souza 566535
-# Lucas Palmeira dos Santos 566204
-# Stephanie Kolln Sanches 564139
+#Bruno Alves de Souza 566535
+#Lucas Palmeira dos Santos 566204
+#Stephanie Kolln Sanches 564139
 
-qt_desastres = int(input('Digite a quantidade de desastres registrados: '))
-i = 0
 
-while i < qt_desastres:
-    print(f"\n--- Desastre {i + 1} ---")
+qt_desastres = int(input('Insira a quantidade de Desastres: '))
+
+for i in range(0, qt_desastres):
+    print()
+    print(f"--- Desastre {i + 1} --- \n")
     tipo_desastre = input('Tipo de desastre: ')
     pais = input('País: ')
     cidade = input('Cidade: ')
     bairro = input('Bairro: ')
     rua = input('Rua: ')
-    qt_pessoas_afetadas = int(input('Quantidade de pessoas afetadas: '))
-    nr_criancas = int(input('Número de crianças: '))
-    nr_adultos = int(input('Número de adultos: '))
-    nr_idosos = int(input('Número de idosos: '))
-    nr_mobilidade_reduzida = int(input('Número de pessoas com mobilidade reduzida: '))
-    nr_feridos = int(input('Número de feridos: \n'))
-    soma_validacao = (nr_feridos + nr_idosos + nr_adultos + nr_criancas + nr_mobilidade_reduzida)
 
-    if qt_pessoas_afetadas != soma_validacao:
-        print("A quantidade total informada diverge da soma dos afetados por grupo!")
-        print("Informe novamente!")
+    while True:
+        qt_pessoas_afetadas = int(input('Quantidade de pessoas afetadas: '))
+        nr_criancas = int(input('Número de crianças: '))
+        nr_adultos = int(input('Número de adultos: '))
+        nr_idosos = int(input('Número de idosos: '))
+        nr_mobilidade_reduzida = int(input('Número de pessoas com mobilidade reduzida: '))
+        nr_feridos = int(input('Número de feridos: '))
+        print()
 
-    else:
-        i = i + 1
+        soma_validacao = nr_criancas + nr_adultos + nr_idosos + nr_mobilidade_reduzida + nr_feridos
+        
+        if qt_pessoas_afetadas != soma_validacao:
+            print("ERRO: A quantidade total informada diverge da soma dos afetados por grupo \n")
+            print("Insira novamente os Dados: \n")
+        else:
+            break
+print()
